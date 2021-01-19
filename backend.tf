9e06358adc323af60788b965902f6d72c051e6db
vikram <vikram.shinde@morrisonsplc.co.uk>
2021-01-19 16:41:48 +0000

    added encryption key

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "project1-service-tfstate"
 //    credentials = "~/credentials/infra-terraform.json"
     prefix = "project1/"
+    encryption_key = "XJMyAttxsczUQKj9vM0w8OuP8Rf4TUbrYwuGGJZ1SRQ="
   }
 }
